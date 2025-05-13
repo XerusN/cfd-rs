99c1d4bee88f8b87e142987b593cd1d5ed99f8d8
XerusN <134720540+XerusN@users.noreply.github.com>
2025-05-13 13:10:08 +0200

    Time integration Category

--- src/finite_volume/discretizations/time_schemes.rs
+++ src/finite_volume/discretizations/time_schemes.rs
@@ -2,3 +2,18 @@
 pub enum TimeIntegration {
     ForwardEuler,
 }
+
+impl TimeIntegration {
+    pub fn time_integration_type(&self) -> TimeIntegrationCategory {
+        match *self {
+            Self::ForwardEuler => TimeIntegrationCategory::Explicit,
+            _ => panic!("TimeIntegrationCategory not defined for {self:?}"),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum TimeIntegrationCategory {
+    Implicit,
+    Explicit,
+}
