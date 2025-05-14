11a6e9865a5cd28eeda48c69d87d52f4995af442
XerusN <134720540+XerusN@users.noreply.github.com>
2025-05-14 14:49:44 +0200

    field update

--- src/finite_volume/base.rs
+++ src/finite_volume/base.rs
@@ -1,6 +1,7 @@
+use cfd_rs_utils::mesh::computational_mesh::Computational2DMesh;
 use nalgebra::Vector2;
 
-use super::gradients::{CellGrad, FaceGrad};
+use super::gradients::GradientConfig;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Field {
@@ -11,7 +12,74 @@ pub enum Field {
 /// The grads will only be allocated if necessary
 #[derive(Debug, PartialEq, Clone)]
 pub struct CellScalarField {
-    pub value: Vec<f64>,
-    pub cell_grad: Vec<Vector2<f64>>,
-    pub face_grad: Vec<Vector2<f64>>
+    values: Vec<f64>,
+    grads_cell: Vec<Vector2<f64>>,
+    grads_face: Vec<Vector2<f64>>,
+    gradients_up_to_date: bool,
+}
+
+impl CellScalarField {
+    
+    pub fn new(cells_num: usize, faces_num: usize, grads_cell_required: bool, grads_face_required: bool) -> Self {
+        let values = vec![0.; cells_num];
+        
+        let grads_cell;
+        if grads_cell_required {
+            grads_cell = vec![Vector2::new(0., 0.); cells_num];
+        } else {
+            grads_cell = vec![];
+        }
+        
+        let grads_face;
+        if grads_face_required {
+            grads_face = vec![Vector2::new(0., 0.); faces_num];
+        } else {
+            grads_face = vec![];
+        }
+        
+        CellScalarField { values, grads_cell, grads_face, gradients_up_to_date: true }
+    }
+    
+    pub fn values(&self) -> &[f64] {
+        &self.values
+    }
+    
+    /// Borrowing mutably the values will set the gradients as not up to date no matter what you do with the values.
+    pub fn values_mut(&mut self) -> &mut [f64] {
+        self.gradients_up_to_date = false;
+        &mut self.values
+    }
+    
+    pub fn grads_cell(&self) -> &[Vector2<f64>] {
+        &self.grads_cell
+    }
+    
+    /// Will not set the gradients as up to date
+    pub fn grads_cell_mut(&mut self) -> &mut [Vector2<f64>] {
+        &mut self.grads_cell
+    }
+    
+    pub fn grads_face(&self) -> &[Vector2<f64>] {
+        &self.grads_face
+    }
+    
+    /// Will not set the gradients as up to date
+    pub fn grads_face_mut(&mut self) -> &mut [Vector2<f64>] {
+        &mut self.grads_face
+    }
+    
+    pub fn gradients_up_to_date(&self) -> bool {
+        self.gradients_up_to_date
+    }
+    
+    /// Updates the gradients to match the values
+    pub fn update_grads(&mut self, mesh: Computational2DMesh, config: GradientConfig) {
+        todo!();
+        self.gradients_up_to_date = true;
+    }
+    
+    /// Will set the gradients as updated with no check
+    pub unsafe fn gradients_updated(&mut self) {
+        self.gradients_up_to_date = true
+    }
 }
--- src/finite_volume/gradients.rs
+++ src/finite_volume/gradients.rs
@@ -1,5 +1,5 @@
 use nalgebra::Vector2;
-use super::{case::Case, equation::*};
+use super::{case::Case, equation::*, interpolations::GradientInterpConfig};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum GradientScheme {
@@ -9,36 +9,7 @@ pub enum GradientScheme {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct FaceGrad {
-    pub var: Variable,
-    pub value: Vec<Vector2<f64>>,
-}
-
-impl FaceGrad {
-    /// Does not compute gradient
-    pub fn new<T: Case>(case: &T, var: Variable) -> FaceGrad {
-        // Generate a warning if no VolGrad is created for the same variable
-        todo!()
-    }
-    
-    pub fn interpolate<T: Case>(&mut self, case: &T) {
-        todo!()
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct CellGrad {
-    pub var: Variable,
-    pub value: Vec<Vector2<f64>>,
-}
-
-impl CellGrad {
-    /// Does not compute gradient
-    pub fn new<T: Case>(case: &T, var: Variable) -> CellGrad {
-        todo!()
-    }
-    
-    pub fn update<T: Case>(&mut self, case: &T) {
-        todo!()
-    }
+pub struct GradientConfig {
+    pub schemes: GradientScheme,
+    pub interp: GradientInterpConfig,
 }
