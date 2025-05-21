473e7b8ee7f315623a1edf1c3f07c5b1a87fde76
XerusN <134720540+XerusN@users.noreply.github.com>
2025-05-21 22:48:27 +0200

    fix boundary definition

--- src/finite_volume/mesh.rs
+++ src/finite_volume/mesh.rs
@@ -1,10 +1,9 @@
-use cfd_rs_mesh::triangle::advancing_front::{self, advancing_front};
+use cfd_rs_mesh::triangle::advancing_front::advancing_front;
 use cfd_rs_utils::{
-    boundary::Boundary,
     control::OutputControl,
     mesh::{
-        computational_mesh::Computational2DMesh,
-        indices::{ParentIndex, VertexIndex},
+        computational_mesh::{BoundaryPatch, Computational2DMesh},
+        indices::{BoundaryPatchIndex, ParentIndex, VertexIndex},
         Modifiable2DMesh, Parent,
     },
 };
@@ -13,12 +12,7 @@ use nalgebra::Point2;
 use super::config::GeometryConfig;
 
 fn square_4_bc() -> Modifiable2DMesh {
-    let parents = vec![
-        Parent::Boundary(Boundary(0)),
-        Parent::Boundary(Boundary(1)),
-        Parent::Boundary(Boundary(2)),
-        Parent::Boundary(Boundary(3)),
-    ];
+    let parents = vec![Parent::Boundary];
     let vertices = vec![
         Point2::new(0.0, 0.0),
         Point2::new(1.0, 0.0),
@@ -27,16 +21,44 @@ fn square_4_bc() -> Modifiable2DMesh {
     ];
 
     let edge_to_vertices_and_parent = vec![
-        (VertexIndex(0), VertexIndex(1), ParentIndex(0)),
-        (VertexIndex(1), VertexIndex(2), ParentIndex(1)),
-        (VertexIndex(2), VertexIndex(3), ParentIndex(2)),
-        (VertexIndex(3), VertexIndex(0), ParentIndex(3)),
+        (
+            VertexIndex(0),
+            VertexIndex(1),
+            (ParentIndex(0), Some(BoundaryPatchIndex(0))),
+        ),
+        (
+            VertexIndex(1),
+            VertexIndex(2),
+            (ParentIndex(0), Some(BoundaryPatchIndex(1))),
+        ),
+        (
+            VertexIndex(2),
+            VertexIndex(3),
+            (ParentIndex(0), Some(BoundaryPatchIndex(2))),
+        ),
+        (
+            VertexIndex(3),
+            VertexIndex(0),
+            (ParentIndex(0), Some(BoundaryPatchIndex(3))),
+        ),
+    ];
+
+    let boundaries = vec![
+        BoundaryPatch::new("Bot".to_string()),
+        BoundaryPatch::new("Right".to_string()),
+        BoundaryPatch::new("Top".to_string()),
+        BoundaryPatch::new("Left".to_string()),
     ];
 
     let mesh;
 
     unsafe {
-        mesh = Modifiable2DMesh::new_from_boundary(vertices, edge_to_vertices_and_parent, parents);
+        mesh = Modifiable2DMesh::new_from_boundary(
+            vertices,
+            edge_to_vertices_and_parent,
+            parents,
+            boundaries,
+        );
     }
 
     mesh
@@ -45,6 +67,6 @@ fn square_4_bc() -> Modifiable2DMesh {
 /// Needs a lot of rework
 pub fn mesh(geometry: &GeometryConfig) -> Computational2DMesh {
     let mut mesh = square_4_bc();
-    advancing_front(&mut mesh, 0.01, OutputControl::Final);
+    advancing_front(&mut mesh, 0.07, OutputControl::None);
     Computational2DMesh::new_from_he(mesh.0)
 }
