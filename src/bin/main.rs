729bf9e58e8d007e109ee58ce28fa652bbb7c994
XerusN <134720540+XerusN@users.noreply.github.com>
2025-05-28 00:08:03 +0200

    test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -112,8 +112,8 @@ fn simple() -> SimpleCase {
 }
 
 fn main() {
-    //let mut case = poisson();
-    let mut case = simple();
+    let mut case = poisson();
+    //let mut case = simple();
 
     for _ in 0..2 {
         case.next_step();
